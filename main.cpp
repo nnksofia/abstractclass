f160c195985e72bc3295d1d63168a5b1f36da3f3
katia <kateryna2506@gmail.com>
2024-03-13 18:02:23 +0200

    reformat

--- main.cpp
+++ main.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
+
 using namespace std;
 
-class Base{
+class Base {
 public:
-    virtual double sum(int x)=0;
+    virtual double sum(int x) = 0;
 };
 
-class Progression: public Base {
+class Progression : public Base {
 private:
     double a;
     double diff;
 public:
-    Progression(double _a, double _diff){
-        a=_a;
-        diff=_diff;
+    Progression(double _a, double _diff) {
+        a = _a;
+        diff = _diff;
     }
-    double sum(int n){
-        double sum=(n+1)*(a+(a+n*diff))/2;
+
+    double sum(int n) {
+        double sum = (n + 1) * (a + (a + n * diff)) / 2;
         return sum;
     }
 };
 
-class Vector: public Base{
+class Vector : public Base {
 private:
     double *v;
 public:
-    Vector(double *_v){
-        v=_v;
+    Vector(double *_v) {
+        v = _v;
     }
+
     double sum(int j) {
-        int sum=0;
-        for(int i=0; i<j; i++){
-            sum=sum+v[i];
+        int sum = 0;
+        for (int i = 0; i < j; i++) {
+            sum = sum + v[i];
         }
         return sum;
     }
@@ -39,17 +42,17 @@ public:
 
 int main() {
 
-    Base* base[3];
+    Base *base[3];
 
     Progression progression(1, 3);
-    double v[3]={1,2, 3};
+    double v[3] = {1, 2, 3};
     Vector vector(v);
 
-    base[0]=&progression;
-    base[1]=&vector;
+    base[0] = &progression;
+    base[1] = &vector;
 
-    base[2]=new Progression (2, 1);
+    base[2] = new Progression(2, 1);
 
-    cout<<base[0]->sum(2)<<endl;
+    cout << base[0]->sum(2) << endl;
     return 0;
 }
